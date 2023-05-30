defde89c755895ccccc6ebecaa5d8988bdb9facf
Giacomo <thejack2307@gmail.com>
2023-05-30 18:11:52 +0200

    instructions and comments

--- Instagram_group_stats.py
+++ Instagram_group_stats.py
@@ -1,28 +1,37 @@
+#import beautiful soup
 from bs4 import BeautifulSoup
 
+#set the user name decodification
 def decode(string):
     if string=="instagram_user_name_partecipant_1":
         return "nickname_1"
     elif string=="instagram_user_name_partecipant_2":
         return "nickname_2"
+    #...
     else:
         return string
-    
+
+#open the group chat file using cp437 decodification, in this way any strange character coming from any message
+#won't generate an error
 with open('message_1.html', encoding="cp437") as data:
+    #create a beautiful soup object
     soup = BeautifulSoup(data, 'html.parser')
 
+#initialise all of the dictionaries that will contain the statistics
 sent_messages = {}
 got_likes = {}
 given_likes = {}
 like_couples_received = {}
 like_couples_given = {}
 
+#list the partecipants
 partecipants = soup.find(class_="pam _3-95 _2ph- _a6-g uiBoxWhite noborder").find(class_="_a6-h").string
 partecipants_names= partecipants.split(": ")[1].split(", ")
-
 for c in range(len(partecipants_names)):
+    #decode the user names of the partecipants accordingly to the nicknames
     partecipants_names[c]=decode(partecipants_names[c])
 
+#add each partecipant as a key to each dictionary
 for name in partecipants_names:
     sent_messages[name]=0
     got_likes[name]=0
@@ -30,29 +39,42 @@ for name in partecipants_names:
     like_couples_received[name]={}
     like_couples_given[name]={}
 
+#initialise the dictionaries that report the interaction statistics of the group members, thanks to this
+#data you will know which users have a similar sense of humor (they exchange more likes between each other)
 for name in partecipants_names:
     for coupled_name in partecipants_names:
         like_couples_received[name][coupled_name]=0
         like_couples_given[name][coupled_name]=0
 
+#find all of the name of the sender of each message in the group
 names_soup = soup.find_all(class_ = '_3-95 _2pim _a6-h _a6-i')
 
+#loop for each message
 for i in names_soup:
+    #add the nickname of who sent the message to the "sent_messages" statistics dictionary
     sender=decode(i.string)
     sent_messages[sender]+=1
 
-    parent=i.parent
-    likes_list=parent.find_all(class_='_a6-q')
-
+    #consider the whole message data with .parent, and go to the likes section by searching for the class "_a6-q"
+    likes_list=i.parent.find_all(class_='_a6-q')
+    #loop for all the likes that the message received
     for j in likes_list:
+        #find all the users that gave a like to the message
         likes=j.find_all("li")
+        #loop though all the user names of who liked the message
         for liker in likes:
-            liker_name=decode(liker.string[6:])
+            #add the like to the "got_likes" dictionary statistics of who sent the message
             got_likes[sender]+=1
+            #retrieve the nickname of who gave the like: we slice the user name excluding the first six characters
+            #because the name is stored after a heart emojy that in the cp437 decodification takes up six characters
+            liker_name=decode(liker.string[6:])
+            #add the like to the "given_likes" dictionary statistics of who gave the like
             given_likes[liker_name]+=1
+            #add the reciprocal interactions to the other dictionaries
             like_couples_received[sender][liker_name]+=1
             like_couples_given[liker_name][sender]+=1
 
+#print the statistics (a lot more can be added)
 for name in partecipants_names:
     print(name.upper())
     print("sent messages:", sent_messages[name])
--- README.md
+++ README.md
@@ -4,25 +4,35 @@ get the statistics of the messages and likes of your friends' group on Instagram
 Have you ever wondered who sends the best memes on the Instagram group you have with your friends? No? Well it doesn't matter.
 Here's how to obtain the statistics of the likes exchanged within an Instagram group. Enjoy.
 
-I'll add the detailed instructions sooner or later
-
 **Disclaimer**: you will work on your private data, do not share your data with anyone and work only on your local machine.
 
-Overview:
-- download the lastest release of the script
-- install the requirements
-- [download your instagram inbox data](#How-to-download-the-instagram-data)
-- [set the nickname decodification](#how-to-set-the-nickname-decodification)
-- share the output with your friends
+**Overview**:
+- Download the [lastest release](https://github.com/kcajj/instagram_groups_statistics/releases) of the script
+- Install the [requirements](#requirements)
+- [Download your instagram inbox data](#How-to-download-the-instagram-data)
+- [Set the nickname decodification](#how-to-set-the-nickname-decodification)
+- Share the output with your friends
 
 ### How to download the instagram data
 Instagram has stored all of your interactions with whatever element of the platform since you created your account. I know it sounds creepy, but it's pretty usefull for this project.
 
-Fortunatly it's very easy to retrieve all of that data, you just go on your profile.........
+Fortunatly it's very easy to retrieve all of that data. If you're on browser you can select the three lines in the bottom left corner and click on your activity. Then you press on "Download your information", at this point instagram will ask you to insert an email address and the desidered file format, select HTML and click next. Instert your passowrd and after a while you should recieve an email containing a zipped folder with all of your data.
 
-Open the directory, go in the messages folder and select for the folder having the name of the instagram group you want to analyse. Extract the message_1.html file into the project directory.
+See the [official instagram guide](https://help.instagram.com/181231772500920/?cms_platform=iphone-app&helpref=platform_switcher) to download the data.
+
+Download the folder and explore it a little, by opening the file "index.html" you will have a broad view of whhich data Instagram usually collects.
+Now go in the messages folder and select for the folder having the name of the instagram group you want to analyse. Extract the message_1.html file and copy it in the project directory.
 
 ### How to set the nickname decodification
 
 some instagram user names may contain non-unicode characters and they become unreadable in the output, a quick solution to this problem is to fill the decode function.
 Set the unreadable user name in the if statement and the chosen nickname in the return statement.
+
+### Requirements
+
+- Python (version 3.10.2)
+- [BeautifulSoup](https://pypi.org/project/beautifulsoup4/) (version 4.11.1)
+
+### More
+
+There are tons of statistics that could be implemented, if you have any idea do not hesitate to add them.
